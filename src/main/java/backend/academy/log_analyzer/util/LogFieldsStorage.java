7ee9df1b02a8d3632af0a30ccb29857bde4a7a65
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-11-10 11:14:15 +0700

    feat(util): добавлен утилитарный класс LogFieldsStorage для хранения имен полей Log

---
+++ src/main/java/backend/academy/log_analyzer/util/LogFieldsStorage.java
@@ -0,0 +1,32 @@
+package backend.academy.log_analyzer.util;
+
+import backend.academy.log_analyzer.log.Log;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import lombok.experimental.UtilityClass;
+
+// Не вижу смысла тестировать
+/**
+ * Класс-хранилище с полями лога (параметрами NGINX лога)
+ */
+@UtilityClass
+public class LogFieldsStorage {
+
+    private static final int COUNT_OF_FIELDS = 8;
+
+    public static final List<String> LOG_FIELDS = Arrays.stream(Log.class.getDeclaredFields())
+        .limit(COUNT_OF_FIELDS)
+        .map(Field::getName)
+        .collect(Collectors.toList());
+
+    public static final List<String> ALL_FIELDS = Stream.concat(
+            LOG_FIELDS.stream(),
+            Arrays.stream(Log.Request.class.getDeclaredFields()
+            ).map(Field::getName)
+        )
+        .collect(Collectors.toList());
+}
+
