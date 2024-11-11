5b7ef37436e1af498dcbf5863f72eb4bc66ed29e
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-11-11 16:46:37 +0700

    feat: добавлен код для запуска приложения

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,11 @@
 package backend.academy;
 
+import backend.academy.log_analyzer.LogAnalyzerApp;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+        new LogAnalyzerApp().startApp(args);
     }
 }
