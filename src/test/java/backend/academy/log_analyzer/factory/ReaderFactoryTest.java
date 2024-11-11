2cb33156e57e80a8d3e1e65a7065376a93daad13
Лапшин Илья Романович <ilia.lapshin2016@yandex.ru>
2024-11-11 19:54:54 +0700

    test(factory): добавлены новые тесты в ReaderFactoryTest
    Добавлены тесты выброса исключений, если подан неверный формат ресурса или не удалось создать ни одного ридера

--- src/test/java/backend/academy/log_analyzer/factory/ReaderFactoryTest.java
+++ src/test/java/backend/academy/log_analyzer/factory/ReaderFactoryTest.java
@@ -1,8 +1,10 @@
 package backend.academy.log_analyzer.factory;
 
+import com.beust.jcommander.ParameterException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,8 +18,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-// Учитывая, что в фабрику попадают провалидированные пути, исключений не должна выкидывать
 @DisplayName("Тесты фабрики ридеров ReaderFactory")
 public class ReaderFactoryTest {
 
@@ -39,9 +41,9 @@ public class ReaderFactoryTest {
         Path dir3 = tempDir.resolve("logsDir/logs/some-logs/2024");
         Path dir4 = tempDir.resolve("logsDir/logs/some-logs/2024_08");
 
-        Files.createFile(dir1.resolve( "logs.txt"));
+        Files.createFile(dir1.resolve("logs.txt"));
 
-        Files.createFile(dir2.resolve( "05-2023.txt"));
+        Files.createFile(dir2.resolve("05-2023.txt"));
         Files.createFile(dir2.resolve("logs-from-url.txt"));
         Files.createFile(dir2.resolve("logs_2024.txt"));
 
@@ -69,26 +71,27 @@ public class ReaderFactoryTest {
 
     @AfterEach
     void closeReaders() {
-        actualReaders.keySet().forEach(r -> {
-            try {
-                r.close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+        if (actualReaders != null) {
+            actualReaders.keySet().forEach(r -> {
+                try {
+                    r.close();
+                } catch (IOException ignore) {
+                }
+            });
+        }
     }
 
     @DisplayName("Тест создания маппы ридеров для нескольких файлов")
     @Test
     void testCreateReadersForManyFiles() throws IOException {
         List<URI> paths = List.of(
-                tempDir.resolve("logsDir/logs/logs.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/05-2023.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/logs-from-url.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/logs_2024.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024/05").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024/05.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024_08/logs").toUri()
+            tempDir.resolve("logsDir/logs/logs.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/05-2023.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/logs-from-url.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/logs_2024.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024/05").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024/05.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024_08/logs").toUri()
         );
 
         actualReaders = ReaderFactory.createReaders(paths);
@@ -102,21 +105,21 @@ public class ReaderFactoryTest {
     @Test
     void testCreateReadersForManyFilesWithSkip() throws IOException {
         List<URI> paths = List.of(
-                tempDir.resolve("logsDir/logs/logs.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/05-2023.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/logs-from-urld.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/logs_2024.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024/05d").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024/05.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024_08/logs").toUri()
+            tempDir.resolve("logsDir/logs/logs.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/05-2023.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/logs-from-urld.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/logs_2024.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024/05d").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024/05.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024_08/logs").toUri()
         );
 
         List<URI> expectedUris = List.of(
-                tempDir.resolve("logsDir/logs/logs.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/05-2023.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/logs_2024.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024/05.txt").toUri(),
-                tempDir.resolve("logsDir/logs/some-logs/2024_08/logs").toUri()
+            tempDir.resolve("logsDir/logs/logs.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/05-2023.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/logs_2024.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024/05.txt").toUri(),
+            tempDir.resolve("logsDir/logs/some-logs/2024_08/logs").toUri()
         );
 
         actualReaders = ReaderFactory.createReaders(paths);
@@ -124,5 +127,23 @@ public class ReaderFactoryTest {
         Collection<URI> actualUris = actualReaders.values();
 
         assertThat(actualUris).hasSize(expectedUris.size()).containsAll(expectedUris);
+    }
+
+    @DisplayName("Тест выброса исключения, если подан неизвестный ресурс")
+    @Test
+    void shouldTrowExceptionBecauseUnknownType() throws URISyntaxException {
+        List<URI> paths = List.of(new URI("ftp://invalid/type"));
+
+        assertThatThrownBy(() -> ReaderFactory.createReaders(paths))
+            .isInstanceOf(ParameterException.class)
+            .hasMessage("Неизвестный вид ресурса");
+    }
+
+    @DisplayName("Тест выброса исключения, если не создан ни один ридер")
+    @Test
+    void shouldThrowExceptionBecauseNoReaders() {
+        assertThatThrownBy(() -> ReaderFactory.createReaders(List.of()))
+            .isInstanceOf(IOException.class)
+            .hasMessage("Не удалось создать ни одного потока чтения файла или URL");
     }
 }
